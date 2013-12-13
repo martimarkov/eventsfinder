031a73611952a70a58eb34718536b23cc2ea74b0
Alejandro Saucedo <axsauze@gmail.com>
2013-12-13 19:49:24 +0000

    Site models fully functional

--- eventsfinder/urls.py
+++ eventsfinder/urls.py
@@ -1,10 +1,23 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
+from django.contrib import admin
+import dbindexer
+from django.conf.urls import patterns
+
+handler500 = 'djangotoolbox.errorviews.server_error'
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    ('^_ah/warmup$', 'djangoappengine.views.warmup'),
+
     url(r'^$', 'eventsfinder.views.home', name='home'),
-    url(r'^add$', 'eventsfinder.views.add_event', name='add_event'),
+    url(r'^create/$', 'eventsfinder.views.add_event', name='add_event'),
+    url(r'^find/$', 'eventsfinder.views.add_event', name='add_event'),
+
+    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
+    
+    ('^admin/', include(admin.site.urls)),
 )
